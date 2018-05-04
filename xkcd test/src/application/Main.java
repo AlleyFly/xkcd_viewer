35894fc18fd6243725ec75c8f9edc81cf65be8bf
AlleyFlyLx <GGnoettgen@gmx.net>
2018-05-04 19:26:00 +0200

    changed title

--- xkcd test/src/application/Main.java
+++ xkcd test/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("xkcd beschte");
+			primaryStage.setTitle("xkcd");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
